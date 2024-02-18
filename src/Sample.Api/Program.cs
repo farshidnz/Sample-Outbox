65ce7656b41c097b9ebcea0324f48c80cba388ef
farshidnz <farshid.shookohi@ospri.co.nz>
2024-02-19 10:42:21 +1300

    local

---
+++ src/Sample.Api/Program.cs
@@ -0,0 +1,123 @@
+using System.Diagnostics;
+using System.Reflection;
+using MassTransit;
+using MassTransit.EntityFrameworkCoreIntegration;
+using MassTransit.Logging;
+using MassTransit.Metadata;
+using Microsoft.EntityFrameworkCore;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Sample.Api;
+using Sample.Components;
+using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("MassTransit", LogEventLevel.Debug)
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
+
+builder.Services.AddScoped<IRegistrationService, RegistrationService>();
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<RegistrationDbContext>(x =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("Default");
+
+    x.UseNpgsql(connectionString, options =>
+    {
+        options.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name);
+        options.MigrationsHistoryTable($"__{nameof(RegistrationDbContext)}");
+
+        options.EnableRetryOnFailure(5);
+        options.MinBatchSize(1);
+    });
+});
+
+builder.Services.AddHostedService<RecreateDatabaseHostedService<RegistrationDbContext>>();
+
+builder.Services.AddOpenTelemetryTracing(x =>
+{
+    x.SetResourceBuilder(ResourceBuilder.CreateDefault()
+            .AddService("api")
+            .AddTelemetrySdk()
+            .AddEnvironmentVariableDetector())
+        .AddSource("MassTransit")
+        .AddAspNetCoreInstrumentation()
+        .AddJaegerExporter(o =>
+        {
+            o.AgentHost = HostMetadataCache.IsRunningInContainer ? "jaeger" : "localhost";
+            o.AgentPort = 6831;
+            o.MaxPayloadSizeInBytes = 4096;
+            o.ExportProcessorType = ExportProcessorType.Batch;
+            o.BatchExportProcessorOptions = new BatchExportProcessorOptions<Activity>
+            {
+                MaxQueueSize = 2048,
+                ScheduledDelayMilliseconds = 5000,
+                ExporterTimeoutMilliseconds = 30000,
+                MaxExportBatchSize = 512,
+            };
+        });
+});
+builder.Services.AddMassTransit(x =>
+{
+    x.AddEntityFrameworkOutbox<RegistrationDbContext>(o =>
+    {
+        o.QueryDelay = TimeSpan.FromSeconds(1);
+
+        o.UsePostgres();
+        o.DisableInboxCleanupService();
+        o.UseBusOutbox(o =>
+        {
+            o.DisableDeliveryService();
+        });
+    });
+    
+    x.UsingInMemory();
+     // x.UsingRabbitMq((context, cfg) =>
+     // {
+     //     cfg.Host("localhost", h =>
+     //     {
+     //         h.Username("guest");
+     //         h.Password("guest");
+     //     });
+     //     cfg.OverrideDefaultBusEndpointQueueName("outbox");
+     //     cfg.AutoStart = true;
+     //     cfg.SingleActiveConsumer = true;
+     // });
+});
+
+builder.Services.AddSingleton(provider => Bus.Factory.CreateUsingRabbitMq(cfg => cfg.Host("localhost", "/", h =>
+    {
+        h.Username("guest");
+        h.Password("guest");
+    }
+))); 
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
